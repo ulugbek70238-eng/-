6c3007a1c4efdfadf3d8a425a3c516a9c4fcbecf
ulugbek70238 <ulugbek70238@gmail.com>
2026-03-03 22:21:19 +0500

    добавил регистрацию и поиск

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('category/<int:pk>', views.categ),
     path('theme/<int:pk>', views.categ),
     path('theme2/<int:pk>', views.theme2),
+    path('register', views.Register.as_view()),
+    path('search', views.search),
+    path('logout', views.logout_view),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category, Theme
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout
+from .forms import RegForm
+from django.views import View
+
 
 
 def home1(request):
@@ -16,3 +21,45 @@ def categ(request, pk):
 def theme2(request, pk):
     theme3 = Theme.objects.get(id=pk)
     return render(request, 'theme.html', {'theme3': theme3})
+
+
+class Register(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        form = RegForm
+        context = {'form': form}
+        return render(request, self.template_name, context)
+
+    def post(self, request):
+        form = RegForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password1')
+
+
+            user = User.objects.create_user(username=username,
+                                            email=email,
+                                            password=password)
+            user.save()
+            login(request, user)
+            return redirect('/')
+
+def search(request):
+    if request.method == 'POST':
+        searched_product = request.POST.get('search_product')
+        product = Theme.objects.filter(name_theme__iregex=searched_product)
+        context = {}
+        if product:
+            context.update(user_pr=searched_product, products=product)
+        else:
+            context.update(user_pr=searched_product, products='')
+        return render(request, 'result.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+
+
